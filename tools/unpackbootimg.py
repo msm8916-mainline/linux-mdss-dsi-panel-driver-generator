b6de028651b349806b49f863a41935997bc55606
lambdadroid <lambdadroid@gmail.com>
2019-08-04 17:33:10 +0200

    tools: Add simple tools to unpack boot/qcdt images

---
+++ tools/unpackbootimg.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# SPDX-License-Identifier: GPL-2.0-only
+import struct
+import sys
+
+BOOT_MAGIC = 'ANDROID!'.encode()
+
+HEADER_FORMAT = '<8s10I16s512s32s1024s'
+HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
+
+
+def extract_file(f, name, pos, size):
+	f.seek(pos)
+	with open(name, 'wb') as o:
+		o.write(f.read(size))
+
+
+def unpack_image(f):
+	header = struct.unpack(HEADER_FORMAT, f.read(HEADER_SIZE)[:HEADER_SIZE])
+
+	# Ensure this is an Android boot image
+	if header[0] != BOOT_MAGIC:
+		print("Image does not appear to be an Android boot image")
+		exit(1)
+
+	page_size = header[8]
+	page_mask = page_size - 1
+
+	offset = page_size
+
+	kernel_size = header[1]
+	if kernel_size:
+		extract_file(f, 'kernel.img', offset, kernel_size)
+		offset += (kernel_size + page_mask) & ~page_mask
+
+	ramdisk_size = header[3]
+	if ramdisk_size:
+		extract_file(f, 'ramdisk.img', offset, ramdisk_size)
+		offset += (ramdisk_size + page_mask) & ~page_mask
+
+	second_size = header[5]
+	if second_size:
+		extract_file(f, 'second.img', offset, second_size)
+		offset += (second_size + page_mask) & ~page_mask
+
+	dtb_size = header[9]
+	if dtb_size > 1:
+		extract_file(f, 'dtb.img', offset, dtb_size)
+		offset += (dtb_size + page_mask) & ~page_mask
+
+	# Extract command line
+	cmdline = header[12].decode().rstrip('\0') + header[14].decode().rstrip('\0')
+
+	with open('cmdline.txt', 'w') as o:
+		o.write(cmdline)
+
+
+with open(sys.argv[1], 'rb') as f:
+	unpack_image(f)
---
+++ tools/unpackqcdt.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import struct
+import sys
+from dataclasses import dataclass
+from typing import Tuple
+
+QCDT_MAGIC = 'QCDT'.encode()
+
+HEADER_FORMAT = '<4sII'
+HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
+
+PAGE_SIZE = 2048
+ENTRY_FORMATS = (
+	'<IIIII',
+	'<IIIIII',
+	'<IIIIIIIIII',
+)
+
+
+@dataclass
+class DTRecord:
+	plat_id: int
+	variant_id: int
+	subtype_id: int
+	offset: int
+	size: int
+	soc_rev: int = 0
+	pmic: Tuple[int, int, int, int] = (0, 0, 0, 0)
+
+
+with open(sys.argv[1], 'rb') as f:
+	b = f.read()
+
+magic, version, n = struct.unpack(HEADER_FORMAT, b[:HEADER_SIZE])
+if magic != QCDT_MAGIC:
+	print("Image does not appear to be an QCDT image")
+	exit(1)
+
+print(f'Version: {version}, Count: {n}')
+
+records = []
+
+offset = HEADER_SIZE
+for i in range(0, n):
+	entry_format = ENTRY_FORMATS[version - 1]
+	size = struct.calcsize(entry_format)
+	entry = struct.unpack(entry_format, b[offset:offset + size])
+	offset += size
+
+	r = DTRecord(entry[0], entry[1], entry[2], entry[-2], entry[-1])
+
+	if version >= 2:
+		r.soc_rev = entry[3]
+	if version >= 3:
+		r.pmic = (entry[4], entry[5], entry[6], entry[7])
+
+	print(r)
+	records.append(r)
+
+for r in records:
+	with open(f'plat_{r.plat_id:x}-var_{r.variant_id:x}-sub_{r.subtype_id:x}.dtb', 'wb') as f:
+		f.write(b[r.offset:r.offset + r.size])
